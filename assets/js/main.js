7faad5c3699be1bdb7ce4f929d221e57e5fd0a46
FloLcs <florianlcs16@gmail.com>
2022-09-11 22:47:46 +0200

    swiper et responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,66 @@ VANTA.TOPOLOGY({
   waveSpeed: 5,
   zoom: 1,
 });
+
+// Swiper JS
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3.2,
+  spaceBetween: 20,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    425: {
+      slidesPerView: 4.2,
+      spaceBetween: 20,
+    },
+    600: {
+      slidesPerView: 5.2,
+      spaceBetween: 40,
+    },
+    768: {
+      slidesPerView: 6.2,
+      spaceBetween: 50,
+    },
+    900: {
+      slidesPerView: 7.2,
+      spaceBetween: 50,
+    },
+    1024: {
+      slidesPerView: 8.2,
+      spaceBetween: 50,
+    },
+    1200: {
+      slidesPerView: 9.2,
+      spaceBetween: 50,
+    },
+    1440: {
+      slidesPerView: 10.2,
+      spaceBetween: 50,
+    },
+  },
+});
+
+// JS
+const swiperOpen = document.getElementById("swiperOpen");
+const swiperClose = document.getElementById("swiperClose");
+const swiperJs = document.querySelector(".swiperjs");
+
+swiperOpen.addEventListener("click", () => {
+  swiperJs.style.display = "flex";
+  swiperOpen.style.display = "none";
+  swiperClose.style.display = "flex";
+});
+
+swiperClose.addEventListener("click", () => {
+  swiperJs.style.display = "none";
+  swiperOpen.style.display = "flex";
+  swiperClose.style.display = "none";
+});
